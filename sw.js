2d360069ae3942c3b85df61456bdfbd4fc3f544e
carcoragu <carcoragu@gmail.com>
2026-06-02 18:28:33 -0300

    v9: diagnostico JS en login-sub, fix Unicode en comentario, sw v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = 'gestfact-v8';
+﻿const CACHE = 'gestfact-v9';
 self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
